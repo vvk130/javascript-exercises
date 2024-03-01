d234c914519ca580995667807ef3266e400da06b
vvk130 <vanessa.kasula@outlook.com>
2024-03-01 22:31:36 +0200

    Exercises

--- javascript_questions.js
+++ javascript_questions.js
@@ -1,1 +1,25 @@
 // Objects in Javascript
+
+// Q: Does delete, delete a value?
+
+const func = (function (a) {
+    delete a;
+    return a;
+})(5);
+
+// A:
+// Delete doesn't delete the value, because it deletes object properties only
+
+// Q: How to add a property?
+
+const user = {
+    name: "Roadside Coder",
+    age: 24,
+    "like this video": true,
+} 
+
+// Q: How to access the property?
+
+console.log();
+
+// delete a property

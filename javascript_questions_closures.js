3bf2535130be6d798770ac071542af4525fa0c08
vvk130 <vanessa.kasula@outlook.com>
2024-03-02 17:34:31 +0200

    Exercise

--- javascript_questions_closures.js
+++ javascript_questions_closures.js
@@ -3,3 +3,34 @@
 // "closure" gives you access to an outer functionäs scope from an inner function
 
 // Closure makes it possible to have private variables in javaacript
+
+function makeFunc(){
+    var name = "Mozilla";
+    function displayName(){
+        console.log(name);
+    }
+    return displayName;
+}
+
+makeFunc()(); // Mozilla
+
+// Q: closure scope chain?
+
+// 3 scopes
+
+// - local scope
+// - outer functions scope
+// - global scope
+
+
+function makeFunc(){
+    var name = "Mozilla";
+    function displayName(){ // this function has also access to global scope
+        console.log(name);
+        // if we had function here it would have access to all scopes
+    }
+    return displayName;
+}
+
+makeFunc()(); // output: Mozilla
+

2801a2019dca91f3faa1318380eb98b98116e633
vvk130 <vanessa.kasula@outlook.com>
2024-03-03 17:07:01 +0200

    Exercise

--- javascript/javascript_questions_call_bind_apply.js
+++ javascript/javascript_questions_call_bind_apply.js
@@ -9,3 +9,65 @@ function sayHello(age) {
 console.log(sayHello.call(obj, 24));
 
 //Q: What is apply?
+
+var obj = { name: "Elli" };
+
+function sayHello(age, profession) {
+  return "Hello " + this.name + " is " + age + " and " + profession;
+}
+
+console.log(sayHello.apply(obj, 24, "Software Developer")); // THIS GIVES AN ERROR
+
+var obj = { name: "Elli" };
+
+function sayHello(age, profession) {
+  return "Hello " + this.name + " is " + age + " and " + profession;
+}
+
+console.log(sayHello.apply(obj, [24, "Software Developer"])); // THIS IS CORRECT
+
+// Q: What is bind?
+
+var obj = { name: "Elli" };
+
+function sayHello(age, profession) {
+  return "Hello " + this.name + " is " + age + " and " + profession;
+}
+
+const bindfunc = sayHello.bind(obj);
+console.log(bindfunc(24, "Dental Hygienist"));
+
+// Q: Call with function inside object
+// What is output?
+
+const age = 10;
+
+var person = {
+  name: "Piyush",
+  age: 20,
+  getAge: function () {
+    return this.age;
+  },
+};
+
+var person2 = { age: 24 };
+person.getAge.call(person2);
+
+// A: 24
+
+//Q: How about apply?
+
+// A: 24
+person.getAge.apply(person2);
+
+
+// Q: How about with bind?
+
+// A: 24
+person.getAge.bind(person2)();
+
+
+
+//Q: 
+
+🎍😍
